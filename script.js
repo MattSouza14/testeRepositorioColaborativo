3ec2d2db26ba2f7825a1d384add84d45f2dbbb85
MARIA EDUARDA SILVA <MARIA0855@DIGITALCOLLEGE.LOCAL>
2024-06-21 16:54:46 -0300

    script.js novo

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Eduarda"    
+let numero 
+
+// alert("Olá " + nome) 
+
+console.log(numero)
+
+// entrega de texto
+let entradaUsuario = prompt("Digite seu CPF")
+numero = confirme("Deseja fazer o seu cartão da C&A") 
+document.write('Olá mundo')
+ 
