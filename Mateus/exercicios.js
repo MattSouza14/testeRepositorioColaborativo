48cca5c54dd6ba681c47c8982562844fd644cd19
MATEUS SOUZA DO NASCIMENTO <MATEUS6268@DIGITALCOLLEGE.LOCAL>
2024-06-24 15:31:57 -0300

    exercicios js

---
+++ Mateus/exercicios.js
@@ -0,0 +1,53 @@
+//  Questão 1
+
+let salarioFuncionario = prompt("Informe o seu salario: ")
+console.log (`O novo salário é ${Number(salarioFuncionario) * 1.2 }R$`)
+
+// Questão 2
+
+
+let nota1 = Number(prompt("Infomre a primeira nota: "))
+
+let nota2 = Number(prompt("Informe a segunda nota: "))
+
+let nota3 = Number(prompt("Informe a terceira nota: "))
+
+
+let media = (nota1 + nota2 + nota3) / 3
+
+console.log(`A média foi de: ${media}`)
+console.log("A média foi de: " + media)
+
+
+
+//Questão 3 
+
+let ladoQuadrado = Number(prompt("Informe o valor do lado do quadrado: "))
+
+let area = ladoQuadrado * ladoQuadrado
+
+console.log (`O valor da área do quadrado é ${area}`)
+
+//Questão 4
+
+let base = Number(prompt("Informe o valor da base:"))
+let altura = Number(prompt("Informe o valor da altura:"))
+
+let areaTriangulo = (base * altura) / 2
+
+console.log(`A área do triangulo é ${areaTriangulo}`)
+
+// Questão 5
+const PI = 3.14
+let raio = Number(prompt("Informe o valor do Raio: "))
+
+let areaCirculo = PI * (raio * raio)
+
+console.log (`A área do cirulo é ${areaCirculo}`)
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("oi")
+// let nome = 'Mateus'
+// let numero
+// let numeroDois = 5
+// let verdadeiroFalso = true
+
+
+// console.log(numero)
+// console.log(nome)
+// console.log(verdadeiroFalso)
+
+
+
+// // alert("Olá " + nome)
+// console.log(typeof(numero)) 
+// console.log(typeof(nome)) 
+// console.log(typeof(verdadeiroFalso)) 
+// console.log(typeof(numeroDois))
+
+// console.log (numero + numeroDois)
+// console.log (numero + 10)
+
+// console.log (numero * numeroDois)
+
+// //Entrada de texto
+
+// let entradaUsuario = prompt("Digite seu cpf: ")
+
+// console.log(entradaUsuario)
+
+
+// numero = confirm('Deseja fazer seu cartão')
+
+
+// document.write
+
+
+
