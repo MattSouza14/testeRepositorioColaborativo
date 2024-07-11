47546577107e800bc0e522fc7d0584f80fc6f76a
MATEUS SOUZA DO NASCIMENTO <MATEUS6268@DIGITALCOLLEGE.LOCAL>
2024-07-11 16:58:05 -0300

    local storage

--- card.js
+++ card.js
@@ -37,19 +37,16 @@ let funcionarios = [
 
 ]
 
-//1) Mostre no console o nome da pessoa e quanto ela recebe em um texto formatado, para todos os itens do array, com uma string formatada.
+
 
 funcionarios.forEach((item)=> console.log(`${item.nome} recebe R$ ${(item.gratificacao + item.salario)} de salário e bonificação`))
 
 
-// 2)Para o mesmo array de objetos pessoas, crie um novo array, com apenas os valores das gratificações
-
 let gratificacoes = funcionarios.map((item)=>item.gratificacao)
 
-// 3)Utilizando o map, crie uma função que retorna um array, com os valores dobrados do array de gratificações.
+
 let gratificacaoDobro = gratificacoes.map((item)=> item * 2)
 
-// 4) Crie um array com as pessoas (objeto) que recebem mais que 4mil reais de salario.
 
 let salariosSup4k =  funcionarios.filter((item)=>{
     if(item.salario>4000){
@@ -57,7 +54,7 @@ let salariosSup4k =  funcionarios.filter((item)=>{
     }
 })
 
-// 5) Crie um array com as pessoas (objeto) que têm mais de 20 anos.
+
 
 let maiores20anos = funcionarios.filter((pessoa)=>{
     if(pessoa.idade>20){
@@ -65,9 +62,12 @@ let maiores20anos = funcionarios.filter((pessoa)=>{
     }
 })
 
-// 6) Exiba o valor total das gratificações. Utilize reduce. 
+
 
 let totalGratificacoes = funcionarios.reduce((total, funcionario)=> {
     total += funcionario.gratificacao
     return total
 },0)
+
+
+
